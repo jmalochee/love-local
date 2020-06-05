9062bc944167e5458ffa502ab92d6028cb48b39a
jmalochee <jmalochee@gmail.com>
2020-06-05 16:40:30 -0400

    added business.about and built out buisness#index

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,8 +1,8 @@
 class Business < ApplicationRecord
 	validates :category, presence: true
   validates :subcategory, presence: true
-  validates :business_type, presence: true
   validates :name, presence: true, length: { minimum: 3 }
+  validates :about, length: { maximum: 250 }
   validates :address, presence: true
   validates :street, presence: true, length: { minimum: 3 }
   validates :city, presence: true, length: { minimum: 3 }
@@ -11,5 +11,5 @@ class Business < ApplicationRecord
   validates :phone, presence: true, length: { is: 10 }, numericality: { only_integer: true }
   validates :website, length: { minimum: 5 }, allow_nil: true
   validates :sqft, numericality: { only_integer: true, greater_than: 0 }, allow_nil: true
-  validates :naics, length: { within: 5..6 }, numericality: { only_integer: true, greater_than: 0 }
+  validates :naics, length: { within: 5..6 }, numericality: { only_integer: true, greater_than: 0 }, allow_nil: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_155937) do
+ActiveRecord::Schema.define(version: 2020_06_05_191945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2020_06_04_155937) do
     t.string "black"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "about"
   end
 
 end
